418cfe677802b61019f65c13fd2f644b940e46df
Jasper Davis <jdavis30@uvm.edu>
2016-04-26 19:47:54 -0400

    generated head object.

---
+++ Head.cpp
@@ -0,0 +1,23 @@
+/*
+ * CS100 Final Project: Tron Snake
+ * Author: Jasper Davis
+ * File: Head.cpp
+ * Date: 4/26/2016
+ * Objective:   Head of the tron snake, defines where the body will go next.
+ */
+
+Head::Head(int dir) {
+	direction = dir;
+}
+
+int Head::getDirection() {
+	return direction;
+}
+
+void Head::setDirection(int dir) {
+	direction = dir;
+}
+
+void draw() {
+	;
+}
---
+++ Head.h
@@ -0,0 +1,25 @@
+/*
+ * CS100 Final Project: Tron Snake
+ * Author: Jasper Davis
+ * File: Head.h
+ * Date: 4/26/2016
+ * Objective:   Head of the tron snake, defines where the body will go next.
+ */
+
+#ifndef HEAD_H
+#define HEAD_H
+#include "Entity.h"
+
+class Head: public Entity {
+	public:
+		Head(int dir = 1);
+		int getDirection();
+		void setDirection(int dir = 0);
+		void draw();
+
+	private:
+		int direction;
+
+};
+
+#endif /** Head_H */
