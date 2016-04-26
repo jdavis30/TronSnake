736579735701e90b5655a50271adcb31008d9794
Jasper Davis <jdavis30@uvm.edu>
2016-04-26 19:52:00 -0400

    produced Game.h

---
+++ Game.h
@@ -0,0 +1,7 @@
+/*
+ * CS100 Final Project: Tron Snake
+ * Author: Jasper Davis
+ * File: Game.h
+ * Date: 4/26/2016
+ * Objective: "Operator" of the entire system. updates variables, instantiates them, and ends the process.
+ */
