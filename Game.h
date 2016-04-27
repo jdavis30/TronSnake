63396f7cd13664237906414c2fd1345c7dd58ada
Jasper Davis <jdavis30@uvm.edu>
2016-04-26 20:17:43 -0400

    Fleshed Game.h.

--- Game.h
+++ Game.h
@@ -3,5 +3,28 @@
  * Author: Jasper Davis
  * File: Game.h
  * Date: 4/26/2016
- * Objective: "Operator" of the entire system. updates variables, instantiates them, and ends the process.
+ * Objective: "Operator" of the entire system. updates variables, draws them, and ends the process.
  */
+#ifndef GAME_H
+#define GAME_H
+#include "Grid.h"
+#include <vector>
+
+class Game {
+	public:
+		Game(int sizeX = 10, int sizeY = 10);
+
+		void update();
+
+		void draw();
+
+	private:
+		int x;
+		int y;
+		Grid grid;
+		Head head;
+		vector<Tail> tail;
+
+};
+
+#endif GAME_H
