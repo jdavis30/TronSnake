8bb47ed8b0e0641c58a6852f6bb110b1ea4c6b9e
CollinGraf314 <cgraf@uvm.edu>
2016-04-26 21:27:58 -0400

    Logic for Game::Update done

--- Game.cpp
+++ Game.cpp
@@ -2,12 +2,17 @@
 // Created by Collin on 4/26/2016.
 //
 
+#include <stdlib.h>
 #include "Entity.h"
+#include "Tail.h"
+#include "Food.h"
 
-void Game::update(){
+bool Game::update(){
+    ///////////// deal with moving head ///////////////
     int dir = head.getDirection();
     int head_x = head.getX();
     int head_y = head.getY();
+    bool ateFood = false;
     Entity moveInTo;
     if(dir == 0){
         if(head_x == 0){
@@ -41,4 +46,35 @@ void Game::update(){
     if (moveInTo.getType() == 0){
         moveInTo = head;
     }
+    else if(moveInTo.getType() == 3){
+        moveInTo = head;
+        ateFood = true;
+    }
+    else{
+        return false;//YOU HIT THE TAIL AND LOST THE GAME!!!!!
+    }
+
+    /////////// Deal with tail ////////////////
+    Tail whereHeadWas = Tail();
+    whereHeadWas.setX(head_x);
+    whereHeadWas.setY(head_y);
+    tail.insert (tail.begin(), whereHeadWas);
+    if(!(ateFood)) {
+        tail.erase(tail.size()-1);
+        grid[head_y*SIZE_X+head_x] = emptyCell;
+    }
+
+    /////////// Deal with eatten food /////////////
+
+    if(ateFood){
+        Food newFood = Food();
+        while(true){
+            index = rand() % (SIZE_X*SIZE_Y-1);
+            if(grid[index].getType() != 0)
+                continue;
+            grid[index] = newFood;
+            break;
+        }
+    }
+    return true;
 }
